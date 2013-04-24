d1d50092f69e04b0a25a6b4442ef1921e6a16203
Brian Svoboda <bsvo@lavabit.com>
2013-04-23 23:31:29 -0700

    Added units module, needs work

--- besl/__init__.py
+++ besl/__init__.py
@@ -7,10 +7,10 @@ besl
 
 """
 
-import bplot, catalog, coord, mathf, misc, prop
+import bplot, catalog, coord, mathf, misc, prop, units
 import bgps_peak_flux_extract
 __all__ = ['bgps_peak_flux_extract', 'bplot', 'catalog', 'coord', 'math',
-           'misc', 'prop']
+           'misc', 'prop', 'units']
 
 __author__ = ['Brian Svoboda, bsvo@lavabit.com']
 __version__ = '0.0.1'
