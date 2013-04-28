9f86f4b568c7940669ef9c589a9b6b7311abbe42
Brian Svoboda <bsvo@lavabit.com>
2013-04-28 14:10:44 -0700

    Added bconfig module

---
+++ besl/bplot/bconfig.py
@@ -0,0 +1,12 @@
+"""
+==============
+bplot Settings
+==============
+
+Variables, dictionaries, and other settings for files in bplot.
+
+"""
+
+import matplotlib.cm as cm
+
+bhexbin_kwargs = {'cmap': cm.cubehelix, 'marginals': True, 'antialiased': True}
