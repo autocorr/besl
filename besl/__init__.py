fc79740be6e63f8f3679a7be945ac994a81868a2
Brian Svoboda <bsvo@lavabit.com>
2013-05-01 18:50:24 -0700

    Updated __init__

--- besl/__init__.py
+++ besl/__init__.py
@@ -8,9 +8,9 @@ besl
 """
 
 import bplot, catalog, coord, mathf, misc, prop, units
-import bgps_peak_flux_extract
-__all__ = ['bgps_peak_flux_extract', 'bplot', 'catalog', 'coord', 'math',
-           'misc', 'prop', 'units']
+import bgps_peak_flux_extract, bgps_finder_charts
+__all__ = ['bgps_peak_flux_extract', 'bgps_finder_charts', 'bplot', 'catalog',
+           'coord', 'math', 'misc', 'prop', 'units']
 
 __author__ = ['Brian Svoboda, bsvo@lavabit.com']
 __version__ = '0.0.1'
