955d2523d3adf3efcedf1cdf975475950a79f4af
Brian Svoboda <bsvo@lavabit.com>
2013-05-14 20:36:38 -0700

    Updated stages plot

--- besl/bplot/stages_plot.py
+++ besl/bplot/stages_plot.py
@@ -8,11 +8,13 @@ Plot the BGPS maps and label-masks overplotted with various catalogs for
 sign-posts of star-formation and evolutionary stage.
 
 """
-
 import aplpy
 import pyfits
+import numpy as _np
 import matplotlib.pyplot as _plt
+import matplotlib.patheffects as PathEffects
 from besl import catalog
+import ipdb as pdb
 
 def overplot_markers(gc):
     """
@@ -81,16 +83,25 @@ def overplot_rind(gc, field):
     -------
     gc APLpy.FITSFigure
     """
+    epsilon = 9e-1 # fudge factor for contouring
     bgps = catalog.read_bgps(exten='all')
-    bgps['glon_pix'], bgps['glat_pix'] = gc.world2pixel(bgps['glon'].values,
-        bgps['glat'].values)
     rind = pyfits.open('/mnt/eld_data/BGPS/Images/v2.0.0/v2.0_ds2_{}_13pca_labelmask.fits'.format(field))
-    # get BGPS field cnum's
-    cnums = bgps.ix[bgps.field == field, 'cnum'].values
-    for cnum in cnums:
-        # for each source, overplot contour
-        cnum_rind = rind
-        gc.show_contour
+    yp, xp = rind[0].data.shape
+    X, Y = _np.meshgrid(_np.arange(xp), _np.arange(yp))
+    field_cnums = bgps.ix[bgps.field == field, 'field_cnum'].values
+    for cnum in field_cnums:
+        Z = _np.zeros((yp, xp))
+        #rind_args = _np.argwhere(
+        #     (rind[0].data[1:-1,1:-1] == cnum) &
+        #    ((rind[0].data[0:-2,1:-1] != cnum) |
+        #     (rind[0].data[2:  ,1:-1] != cnum) |
+        #     (rind[0].data[1:-1,0:-2] != cnum) |
+        #     (rind[0].data[1:-1,2:  ] != cnum)))
+        rind_args = _np.argwhere(rind[0].data == cnum)
+        Z[rind_args[:,0] + 1, rind_args[:,1] + 1] = 1
+        _plt.contour(X, Y, Z, levels=[epsilon], colors='0.25', linewidths=2,
+            zorder=-1)
+    rind.close()
     # TODO overplot green contours for starless clumps
     return gc
 
@@ -107,43 +118,63 @@ def overplot_cnums(gc, field):
     -------
     gc APLpy.FITSFigure
     """
-    bgps = besl.catalog.read_bgps(exten='all')
-    bgps['glon_pix'], bgps['glat_pix'] = gc.world2pixel(bgps['glon'].values,
-        bgps['glat'].values)
-    bgps['cnum_str'] = bgps['cnum'].apply(str)
-    rind = pyfits.open('/mnt/eld_data/BGPS/Images/v2.0.0/v2.0_ds2_{}_13pca_labelmask.fits'.format(field))
+    bgps = catalog.read_bgps()
+    bgps['glon_pix'], bgps['glat_pix'] = gc.world2pixel(bgps['glon_cen'].values,
+        bgps['glat_cen'].values)
+    bgps['cnum_str'] = bgps['cnum'].apply(int).apply(str)
     # get BGPS field cnum's
     bgps_select = bgps.ix[bgps.field == field].reset_index()
     cnums = bgps.ix[bgps.field == field, 'cnum'].values
     # for each source, overplot marker
     for i in xrange(bgps_select.shape[0]):
-        txt = _plt.annotate(bgps.ix[i, 'cnum_str'], xy=(bgps.ix[i, 'glon_pix'],
-            bgps.ix[i, 'glat_pix']), xytext=(3.75,-2.5), xycoords='data',
-            textcoords='offset points', fontsize='6', weight='book',
-            color='grey')
+        txt = _plt.annotate(bgps_select.ix[i, 'cnum_str'],
+            xy=(bgps_select.ix[i, 'glon_pix'], bgps_select.ix[i,
+            'glat_pix']), xytext=(3.75,-2.5), xycoords='data',
+            textcoords='offset points', fontsize='7', weight='book',
+            color='black', alpha=0.75, zorder=0)
         txt.set_path_effects([PathEffects.withStroke(linewidth=2,
-            foreground='w')])
+            foreground='w', alpha=0.75)])
     # TODO overplot green cnum labels for starless clumps
     return gc
 
 def create_stages_plot(lon, lat, dlon, dlat, out_filen='sign_posts'):
-    # check if figure is in BGPS
+    """
+    Create two panel figure of BGPS label mask and BGPS image in a specified
+    region.
+
+    Parameters
+    ----------
+    lon : number
+        Longitude center coordinate
+    lat : number
+        Latitude center coordinate
+    dlon : number
+        Longitude width
+    dlat : number
+        Latitude height
+    out_filen : string, default 'sign_posts'
+
+    Returns
+    -------
+    gc APLpy.FITSFigure
+    """
+    ### check if figure is in BGPS
     field_check = catalog.select_bgps_field(lon, lat, coord_type='gal',
             bool_out=True)
     if not field_check:
         raise Exception('Not in BGPS maps.')
     field = catalog.select_bgps_field(lon, lat, coord_type='gal')
     fig = _plt.figure(figsize=(10, 5))
-    # Label mask
+    ### Label mask
     gc1 = aplpy.FITSFigure(
         '/mnt/eld_data/BGPS/Images/v2.0.0/v2.0_ds2_{}_13pca_labelmask.fits'.format(field),
         figure=fig, subplot=(1,2,1), convention='calabretta')
     gc1.show_colorscale(cmap='gist_gray', vmin=-0.15, vmax=0.05,
         stretch='linear')
     gc1.recenter(lon, lat, width=dlon, height=dlat)
-    gc1 = overplot_rind(gc1, field)
     gc1 = overplot_cnums(gc1, field)
-    # BGPS Image
+    gc1 = overplot_rind(gc1, field)
+    ### BGPS Image
     gc2 = aplpy.FITSFigure(
         '/mnt/eld_data/BGPS/Images/v2.0.0/v2.0_ds2_{}_13pca_map20.fits'.format(field),
         figure=fig, subplot=(1,2,2), convention='calabretta')
@@ -152,14 +183,16 @@ def create_stages_plot(lon, lat, dlon, dlat, out_filen='sign_posts'):
     gc2.recenter(lon, lat, width=dlon, height=dlat)
     gc2.hide_ytick_labels()
     gc2.hide_yaxis_label()
-    # Colorbar
+    ### Colorbar
     #gc2.add_colorbar(location='right')
     gc_list = [gc1, gc2]
     for gc in gc_list:
-        # Overplot
+        ### Overplot
         gc = overplot_markers(gc)
         #gc = overplot_ellipses(gc)
-        # Grid and ticks
+        #gc = overplot_cnums(gc, field)
+        #gc = overplot_rind(gc, field)
+        ### Grid and ticks
         gc.ticks.set_color('black')
         gc.set_tick_xspacing(0.1)
         gc.set_tick_yspacing(0.1)
@@ -167,8 +200,9 @@ def create_stages_plot(lon, lat, dlon, dlat, out_filen='sign_posts'):
         gc.set_tick_labels_yformat('dd.d')
         gc.add_beam(facecolor='yellow', linewidth=2, hatch='//', major=0.00917,
             minor=0.00917, angle=0)
-    gc.save(out_filen + '.eps', dpi=300)
-    gc.save(out_filen + '.png', dpi=300)
+    ### Save
+    gc2.save(out_filen + '.eps', dpi=300)
+    gc2.save(out_filen + '.png', dpi=300)
     print '-- Printed to file {}'.format(out_filen)
     return gc
 
