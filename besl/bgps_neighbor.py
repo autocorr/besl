1101b7cdb2f63aed933581774c073eea8520c28a
Brian Svoboda <bsvo@lavabit.com>
2013-05-22 16:18:57 -0700

    Added neighbor

---
+++ besl/bgps_neighbor.py
@@ -0,0 +1,15 @@
+"""
+=====================
+BGPS Nearest Neighbor
+=====================
+
+Match and analyze BGPS nearest neighbors in label masks.
+
+"""
+
+import pyfits
+import numpy as _np
+import pandas as _pd
+import catalog, image
+
+
