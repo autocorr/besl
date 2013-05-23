c1d23de7c0bb5a1341db52b2c63fb5186ccb1485
Brian Svoboda <bsvo@lavabit.com>
2013-05-22 22:58:16 -0700

    Work on bgps neighbor

--- besl/bgps_neighbor.py
+++ besl/bgps_neighbor.py
@@ -12,4 +12,27 @@ import numpy as _np
 import pandas as _pd
 import catalog, image
 
+def find_clump_neighbors(cnum, v=201):
+    """
+    Determine the neighbors of a clump by if there is adjacent overlap in the
+    label masks.
+
+    Parameters
+    ----------
+    cnum : number
+        BGPS source catalog number
+    v : number, default 201
+        BGPS version number. Available options:
+            2   -> BGPS v2.0.0
+            201 -> BGPS v2.0.1
+
+    Returns
+    -------
+    neighbors : list
+        List of neighbor's catalog numbers. If no neighbors, returns an empty
+        list.
+    """
+    if v not in [2, 201]:
+        raise ValueError
+    return
 
--- besl/image.py
+++ besl/image.py
@@ -17,11 +17,11 @@ class Dirs(object):
     """
     def __init__(self):
         self.root_dir = '/mnt/eld_data/'
-        self.bgps_dir = self.root_dir + 'BGPS/Images/v2.0.0/'
+        self.bgps_dir = self.root_dir + 'BGPS/Images/{}/'
         self.bgps_img_filen = 'v2.0_ds2_{}_13pca_{}.fits'
 d = Dirs()
 
-def get_bgps_img(cnum, exten):
+def get_bgps_img(cnum, exten, v=201):
     """
     Retrieve BGPS image file in pyfits format. Only works for v2.
 
@@ -35,17 +35,24 @@ def get_bgps_img(cnum, exten):
         map20      -> default map
         medmap20   -> median map 20
         noisemap20 -> rms map
+    v : number, default 2
+        BGPS version number, valid [2, 201]. This only effects choice in label
+        mask.
 
     Returns
     -------
     img : pyfits.HDUList
     """
+    if v not in [2, 201]:
+        raise ValueError
     if exten not in ['labelmask', 'map20', 'medmap20', 'noisemap20']:
         raise ValueError('Incorrect exten: {}.'.format(exten))
-    bgps = catalog.read_bgps(exten='none')
+    ver_path = {2: 'v2.0.0', 201: 'v2.0.1'}
+    bgps = catalog.read_bgps(exten='none', v=v)
     c_index = _np.argwhere(bgps.cnum == cnum)[0][0]
     field = bgps.ix[c_index, 'field']
-    img = pyfits.open(d.bgps_dir + d.bgps_img_filen.format(field, exten))
+    img = pyfits.open(d.bgps_dir.format(ver_path[v]) +
+        d.bgps_img_filen.format(field, exten))
     return img
 
 
