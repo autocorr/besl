caa0c73310d7504266365e14b3209c8556759e2c
Brian Svoboda <bsvo@lavabit.com>
2013-05-23 17:20:41 -0700

    Typo..

--- besl/__init__.py
+++ besl/__init__.py
@@ -9,7 +9,7 @@ besl
 
 import bplot, catalog, coord, dpdf_calc, image, mathf, misc, prop, units
 import bgps_peak_flux_extract, bgps_clump_match_all, bgps_clump_area, \
-       bpps_neighbor
+       bgps_neighbor
 __all__ = ['bgps_peak_flux_extract',
            'bgps_clump_match_all',
            'bgps_clump_area',
