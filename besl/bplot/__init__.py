8d2e98e516430b5ce0f568f0e12fa6d29079c1bb
Brian Svoboda <bsvo@lavabit.com>
2013-07-01 14:27:59 -0700

    Add ktemp module to init

--- besl/bplot/__init__.py
+++ besl/bplot/__init__.py
@@ -6,7 +6,7 @@ bplot
 """
 
 import h2o_all_comparison, bgps_bounds, bconfig, gen_plots, \
-       bgps_finder_charts, stages_plot
+       bgps_finder_charts, stages_plot, ktemp_nh3
 
 __all__ = ['h2o_all_comparison', 'bgps_bounds', 'bconfig', 'gen_plots',
-           'bgps_finder_charts']
+           'bgps_finder_charts', 'ktemp_nh3']
