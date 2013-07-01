9458d4bebbb235ec307c97b0d6ef8173ff84a67d
Brian Svoboda <bsvo@lavabit.com>
2013-07-01 15:22:32 -0700

    Minor edits

--- besl/util.py
+++ besl/util.py
@@ -8,13 +8,14 @@ General routines and utility functions
 """
 
 import pandas as _pd
-import catalog
+from catalog import read_bgps
 
 def bgps_import_check(bgps, exten='all'):
     if len(bgps) == 0:
-        catalog.read_bgps(exten=exten)
-        return bgps
+        # If empty return new BGPS catalog
+        return read_bgps(exten=exten)
     elif isinstance(bgps, _pd.core.frame.DataFrame):
+        # Return non-empty pandas DataFrame
         return bgps
     else:
         raise Exception("Not a pandas DataFrame: {0}".format(type(bgps)))
