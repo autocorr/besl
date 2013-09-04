7bce9edb2d18e45c175e12483dac7f6b8d3c0a8b
Brian Svoboda <bones253@gmail.com>
2013-09-03 17:36:12 -0700

    Working rtree

--- besl/ppv_group.py
+++ besl/ppv_group.py
@@ -8,12 +8,13 @@ molecular line survey.
 
 """
 
+import numpy as np
 from rtree import index
 from .catalog import read_bgps_vel
 
 
 def build_tree(df=None, cols=['glon_peak','glat_peak','vlsr'],
-        flag_col='vlsr_f'):
+        flag_col='vlsr_f', box_size=[0.2, 0.2, 6]):
     """
     Build the R-tree datastructure for the PPV values. If the function is not
     passed any arguments, then the BGPS HCO+/N2H+ catalog is used.
@@ -26,22 +27,30 @@ def build_tree(df=None, cols=['glon_peak','glat_peak','vlsr'],
         decimal degrees and km/s.
     flag_col : str
         Flag column to select good velocities from.
+    box_size : list
+        Query region for each data point, specifies the halfwidth of the box.
 
     Returns
     -------
-    ix3d : rtree.
+    ix3d : rtree.index.Index
     """
     # Read in BGPS
     if df is None:
         df = read_bgps_vel()
-    df = df[(bgpv[cols[2]].notnull()) & (bgpv[flag_col] > 0)]
-    # Create index
+    if flag_col is None:
+        df = df[(df[cols[2]].notnull())]
+    else:
+        df = df[(df[cols[2]].notnull()) & (df[flag_col] > 0)]
+    # Index properties
     props = index.Property()
     props.dimension = 3
     ix3d = index.Index(properties=props)
     # Add coordinates
+    box_size = np.array(box_size)
     for ii in df.index:
-        ix3d.add(ii, df.ix[ii, cols].values)
+        coords = df.ix[ii, cols].values
+        box = np.ravel([coords - box_size, coords + box_size])
+        ix3d.insert(ii, box)
     return ix3d
 
 
