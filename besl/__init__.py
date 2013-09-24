87d7a1b6cec07194b093745225bbc52328dc8143
Brian Svoboda <bones253@gmail.com>
2013-09-23 19:05:59 -0700

    Updates for pathes

--- besl/__init__.py
+++ besl/__init__.py
@@ -7,24 +7,12 @@ besl
 
 """
 
-import bplot, catalog, coord, image, mathf, misc, prop, units, util
-import bgps_peak_flux_extract, bgps_clump_match_all, bgps_clump_area, \
-       bgps_neighbor, dpdf_calc, ppv_group
-__all__ = ['bgps_peak_flux_extract',
-           'bgps_clump_match_all',
-           'bgps_clump_area',
-           'bgps_neighbor',
-           'bplot',
-           'catalog',
-           'coord',
-           'dpdf_calc',
-           'image',
-           'math',
-           'misc',
-           'ppv_group',
-           'prop',
-           'units',
-           'util']
+import (bgps_clump_area, bgps_clump_match_all, bgps_neighbor,
+        bgps_peak_flux_extract, bplot, catalog, coord, dpdf_calc, image, mathf,
+        misc, paths, ppv_group prop, units, util)
+__all__ = ['bgps_peak_flux_extract', 'bgps_clump_match_all', 'bgps_clump_area',
+           'bgps_neighbor', 'bplot', 'catalog', 'coord', 'dpdf_calc', 'image',
+           'math', 'misc', 'paths', 'ppv_group', 'prop', 'units', 'util']
 
 __author__ = ['Brian Svoboda, bsvo@lavabit.com']
 __version__ = '0.0.1'
