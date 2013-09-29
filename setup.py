80f92c7b122d3b8f462ec36dffb1cec38f4d300a
Brian Svoboda <bones253@gmail.com>
2013-09-29 15:28:31 -0700

    Modify requirements

--- setup.py
+++ setup.py
@@ -6,20 +6,21 @@ from besl import __version__ as version
 
 setup(name='besl',
       version=version,
-      description='besl - Brian Svobodda Library, a module of astronomy related routines',
+      description='besl - Brian Svobodda Library, a module of astronomy '
+                  'related routines',
       author=['Brian Svoboda'],
-      author_email=['bsvo@lavabit.com'],
+      author_email=['svobodb@email.arizona.edu'],
       url='https://github.com/autocorr/besl',
       packages=['besl'],
       provides=['besl'],
       cmdclass={'build_py': build_py},
       keywords=['Scientific/Engineering', 'Astronomy'],
-      classifiers=[
-                   "Development Status :: 2 - Pre-alpha",
+      classifiers=["Development Status :: 2 - Pre-alpha",
                    "Intended Audience :: Science/Research",
                    "Natural Language :: English",
                    "Operating System :: Unix",
                    "Programming Language :: Python",
-                   "License :: OSI Approved :: GNU General Public Licence v3 (GPLv3)",
-                  ],
-      )
+                   "License :: OSI Approved :: "
+                   "GNU General Public Licence v3 (GPLv3)"],
+      requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'astropy',
+                'photutils', 'pyephem'])
