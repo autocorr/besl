43ecdc229919435aebf5d36b9c1c2883ecf78f04
Brian Svoboda <bones253@gmail.com>
2013-11-07 21:46:10 -0700

    Working temperature distrib calculator

--- besl/dpdf_mc.py
+++ besl/dpdf_mc.py
@@ -10,6 +10,7 @@ composite, posterior DPDFs for nodes in PPV-groups.
 
 import numpy as np
 from .catalog import read_cat, read_dpdf
+from .mathf import pd_weighted_mean
 from scipy.stats import gaussian_kde
 
 
@@ -70,43 +71,46 @@ class PropCollection(object):
 
 
 class TempDistribs(object):
+    v = 'v210'
     temp_cols = ['nh3_gbt_tkin', 'mol_nh3_tk']
     err_cols = ['nh3_gbt_tkin_err', 'mol_nh3_tk_err']
+    out_cols = ['tk', 'tk_err']
 
     def __init__(self, stages):
         self.stages = stages
+        self.good_tkins = []
+        self.tk_fn = []
 
-    def get_good_tkins(self):
+    def get_good_tkins(self, weight=True):
         for stage in self.stages:
             # NH3 Obs from Rosolowsky
             gbt_mask = (stage['nh3_gbt_snr11'] > 5) & \
-                       (stage['nh3_gbt_snr22'] > 5)
-            gbt = stage[gbt_mask][['nh3_gbt_tkin', 'nh3_gbt_tkin_err']]
+                       (stage['nh3_gbt_snr22'] > 3)
+            gbt = stage[gbt_mask][['v210cnum', 'nh3_gbt_tkin',
+                                   'nh3_gbt_tkin_err']]
             # NH3 Obs collected from Shirley et al. (2013)
             mol_mask = stage['mol_nh3_tk'].notnull()
-            mol = stage[mol_mask][['mol_nh3_tk', 'mol_nh3_tk_err']]
-            temps = gbt.merge(mol, how='outer')
-            # Average temperatures
-            temps['tk'] = temps[self.temp_cols].min(axis=1)
-            temps['tk_err'] = temps[self.err_cols].min(axis=1)
-            both_mask = gbt_mask & mol_mask
-            # Hack for weighted mean
-            temps['tk'][both_mask] = \
-                (temps['nh3_gbt_tkin'] / temps['nh3_gbt_tkin_err'] +
-                 temps['mol_nh3_tk'] / temps['mol_nh3_tk_err']) / \
-                (1 / temps['nh3_gbt_tkin_err'] + 1 / temps['mol_nh3_tk_err'])
-            temps['tk_err'][both_mask] = \
-                np.sqrt(temps['nh3_gbt_tkin_err']**2 +
-                        temps['mol_nh3_tk_err']**2) / 2.
-            self.good_tkins = temps
+            mol = stage[mol_mask][[self.v + 'cnum', 'mol_nh3_tk',
+                                   'mol_nh3_tk_err']]
+            temps = gbt.merge(mol, how='outer', on=self.v + 'cnum')
+            # Weighted mean temperatures
+            if weight:
+                temps = pd_weighted_mean(temps, self.temp_cols, self.err_cols,
+                                         out_cols=self.out_cols)
+            # Use GBT over collected
+            else:
+                temps['tk'] = temps[self.temp_cols[1]]
+                temps['tk'][temps[self.temp_cols[0]].notnull()] = \
+                    temps[self.temp_cols[0]]
+                temps['tk_err'] = temps[self.err_cols[1]]
+                temps['tk_err'][temps[self.err_cols[0]].notnull()] = \
+                    temps[self.temp_cols[0]]
+            self.good_tkins.append(temps)
 
     def to_distrib(self):
-        self.tk_fn = []
-        for stage in self.stages:
-            stage_temps = self.good_tkins.ix[stage.index, ['tk', 'tk_err']]
-            tk_samples = np.random.normal(stage_temps['tk'].values,
-                                          stage_temps['tk_err'].values, 100)
-            tk_samples.sort()
-            kernel = gaussian_kde(tk_samples)
-            kernel.set_bandwidth(stage_temps['tk_err'].median())
+        for temps in self.good_tkins:
+            kernel = gaussian_kde(temps[temps['tk'].notnull()]['tk'])
+            kernel.set_bandwidth(temps['tk_err'].median())
             self.tk_fn.append(kernel)
+
+
