81bf086f944d821d8c1ec97383adc9e59ffef942
Brian Svoboda <bones253@gmail.com>
2013-11-07 19:30:11 -0700

    Add weighted mean function

--- besl/mathf.py
+++ besl/mathf.py
@@ -164,3 +164,44 @@ def ellipse_circumf(major, minor, semi=True):
         return 2. * major * ellipe(eccen2)
 
 
+def pd_weighted_mean(df, val_cols, err_cols, out_cols=('val', 'err')):
+    """
+    Weighted mean of a DataFrame for multiple columns in both values and
+    associated uncertainties. Inverse uncertainties are used as weights. The
+    full DataFrame is returned with columns added for weighted values and
+    weighted errors.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing value and error columns
+    val_cols : list-like
+        Column names for values
+    err_cols : list-like
+        Column names for associated errors in the same order as `val_cols`.
+    out_cols : tuple, default ('val', 'err')
+        Default names for weighted columns
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Full DataFrame with weighted column names added
+    """
+    assert len(val_cols) > 1
+    assert len(err_cols) > 1
+    errs_to_vals = {e: v for v, e in zip(val_cols, err_cols)}
+    # Sum of weights
+    sum_weights = (1. / df[err_cols]).sum(axis=1)
+    # Weighted values
+    # note: `.div` automatically divides columns of the same name
+    #       and axis 1 is by row
+    weighted_vals = df[val_cols].div(
+                    df[err_cols].rename(columns=errs_to_vals)).sum(axis=1)
+    df[out_cols[0]] = weighted_vals.div(sum_weights)
+    # Weighted errors by adding in quadrature
+    nerrs = df[err_cols].count(axis=1).astype(float)
+    nerrs[nerrs == 0] = _np.nan
+    df[out_cols[1]] = (_np.sqrt(df[err_cols].pow(2).sum(axis=1))).div(nerrs)
+    return df
+
+
