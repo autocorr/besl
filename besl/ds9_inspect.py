f0189b2ae2fd674a73b7f3acd08fba9b7d91dc94
Brian Svoboda <bones253@gmail.com>
2013-11-10 12:37:20 -0700

    Base inspector

--- besl/ds9_inspect.py
+++ besl/ds9_inspect.py
@@ -5,23 +5,47 @@ import os
 import ds9
 from astropy import wcs
 from astropy.io import fits
-import numpy as np
-import pandas as pd
+from .image import get_bgps_img
+
+
+class Ds9FrameError(Exception):
+    pass
 
 
 class Inspector(object):
     rind_contour_color = 'yellow'
+    v = 210
 
     def __init__(self, cnum):
         self.cnum = cnum
         self.d = ds9.ds9()
 
-    def _get_rind(self)
-        pass
+    def _get_frame_ids(self):
+        frame_ids = self.d.get('frame all')
+        frame_ids = [int(i) for i in frame_ids.split()]
+        return frame_ids
 
-    def show_rind(self):
-        self._get_find()
-        self.d.set('frame 2')
+    def _get_next_frame_id(self):
+        frame_ids = self._get_frame_ids()
+        next_frame_id = max(frame_ids) + 1
+        if next_frame_id > 38:
+            raise Ds9FrameError
+        return next_frame_id
+
+    def _get_rind(self):
+        rind = get_bgps_img(self.cnum, exten='labelmask', v=self.v)
+        rind[0].data[rind[0].data != self.cnum] = 0
+        return rind
+
+    def _get_flux(self):
+        flux = get_bgps_img(self.cnum, exten='map20', v=self.v)
+        return flux
+
+    def show_source_contour(self):
+        rind = self._get_rind()
+        fid = self._get_next_frame_id()
+        self.d.set('frame {0}'.format(fid))
+        self.d.set_pyfits(rind)
         self.d.set('contour limits 0 1')
         self.d.set('contour nlevels 1')
         self.d.set('contour smooth 1')
@@ -30,7 +54,24 @@ class Inspector(object):
         self.d.set('contour copy')
         self.d.set('frame 1')
         self.d.set('contour paste {0}'.format(self.rind_contour_color))
-        self.d.set('frame delete 2')
+        self.d.set('frame delete {0}'.format(fid))
+
+    def show_flux_contour(self, clevels):
+        fid = self._get_next_frame_id()
+        self.d.set('frame {0}'.format(fid))
+        flux_img = self._get_flux()
+        self.d.set_pyfits(flux_img)
+        self.d.set('contour nvelels {0}'.format(len(clevels)))
+        self.d.set('contour levels {' + ' '.join([str(i) for i in clevels]) + '}')
+        self.d.set('contour smooth 2')
+        self.d.set('contour')
+        self.d.set('contour copy')
+        self.d.set('frame 1')
+        self.d.set('contour paste cyan')
+        self.d.set('frame delete {0}'.format(fid))
+
+    def zoom(self, xcen, ycen, xwidth, ywidth):
+        pass
 
 
 class HiGalInspector(Inspector):
@@ -38,13 +79,16 @@ class HiGalInspector(Inspector):
     Visually inspect HiGal cutouts in DS9.
     """
     # Directories and file names
-    self.root = '/mnt/eld_data/HiGal/'
-    self.data = os.path.join(self.root, 'dat_files')
-    self.data_file = os.path.join(self.data,
+    root = '/mnt/eld_data/HiGal/'
+    data = os.path.join(self.root, 'dat_files')
+    data_file = os.path.join(self.data,
         'source_{cnum}_blue_svoboda_photall_err.dat')
-    self.img = os.path.join(self.root, 'img_files')
-    self.img_file = os.path.join(self.img,
+    img = os.path.join(self.root, 'img_files')
+    img_file = os.path.join(self.img, '{img_type}',
         '{img_type}_source_{cnum}_blue_svoboda.fits')
+    # Properties
+    img_types = ['allder2', 'der1x', 'der1y', 'der2x', 'der2x45', 'der2y',
+                 'der2y45', 'mask_1.']
 
     def __init__(self, cnum):
         super(HigalInspector, self).__init__(cnum)
