b12660edfc6bbedfe2553ffb8056298e0f54bdb4
Brian Svoboda <svobodb@email.arizona.edu>
2014-05-14 16:58:00 -0700

    Remove imports for matplotlib GTKAgg X11 issues on SSH

--- besl/__init__.py
+++ besl/__init__.py
@@ -3,16 +3,11 @@
 besl
 ====
 
-.. moduleauthor :: Brian Svoboda <bsvo@lavabit.com>
+.. moduleauthor :: Brian Svoboda <svobodb@email.arizona.edu>
 
 """
 
-from . import (bgps_clump_area, bgps_clump_match_all, bgps_neighbor,
-               bgps_peak_flux_extract, bplot, catalog, coord, dpdf_calc, image,
-               mathf, misc, paths, ppv_group,prop, units, util)
-__all__ = ['bgps_peak_flux_extract', 'bgps_clump_match_all', 'bgps_clump_area',
-           'bgps_neighbor', 'bplot', 'catalog', 'coord', 'dpdf_calc', 'image',
-           'math', 'misc', 'paths', 'ppv_group', 'prop', 'units', 'util']
+__all__ = ['bplot', 'catalog', 'coord', 'image', 'math', 'misc', 'util']
 
-__author__ = ['Brian Svoboda, bsvo@lavabit.com']
-__version__ = '0.0.1'
+__author__ = ['Brian Svoboda, svobodb@email.arizona.edu']
+__version__ = '0.0.2'
