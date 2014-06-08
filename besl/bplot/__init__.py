cf64de5c5ac1496a8da7ca6abad726a2b148479f
Brian Svoboda <svobodb@email.arizona.edu>
2014-06-08 01:15:00 -0700

    Remove default imports

--- besl/bplot/__init__.py
+++ besl/bplot/__init__.py
@@ -5,10 +5,6 @@ bplot
 
 """
 
-from . import (h2o_all_comparison, bgps_bounds, bconfig, dpdf_plots,
-               galaxy_hist, gen_plots, bgps_finder_charts, size_linewidth,
-               stages_plot, ktemp_nh3, ppv_group_plots, bmpl)
-
 __all__ = ['h2o_all_comparison', 'bgps_bounds', 'bconfig', 'dpdf_plots',
            'galaxy_hist', 'gen_plots', 'bgps_finder_charts', 'size_linewidth',
            'stages_plot', 'ktemp_nh3', 'ppv_group_plots', 'bmpl']
