f78e6196a9ba786394b7b91bfcc4326ad51fbba5
Brian Svoboda <svobodb@email.arizona.edu>
2014-11-27 03:50:47 -0700

    Remove rtree function

--- besl/ppv_group.py
+++ besl/ppv_group.py
@@ -15,50 +15,8 @@ import cPickle as pickle
 from collections import deque
 from multiprocessing import Pool
 from scipy.special import erf
-from rtree import index
 from .catalog import read_bgps_vel, read_cat, read_dpdf
 from .coord import sep
-
-
-def build_tree(df=None, cols=('glon_peak','glat_peak','vlsr'),
-        flag_col='vlsr_f', box_size=(0.2, 0.2, 6)):
-    """
-    Build the R-tree datastructure for the PPV values. If the function is not
-    passed any arguments, then the BGPS HCO+/N2H+ catalog is used.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-    cols : list
-        List of column names for Galactic longitude, latitude, and velocity in
-        decimal degrees and km/s.
-    flag_col : str
-        Flag column to select good velocities from.
-    box_size : tuple
-        Query region for each data point, specifies the halfwidth of the box.
-
-    Returns
-    -------
-    ix3d : rtree.index.Index
-    """
-    # Read in BGPS
-    if df is None:
-        df = read_bgps_vel()
-    if flag_col is None:
-        df = df[(df[cols[2]].notnull())]
-    else:
-        df = df[(df[cols[2]].notnull()) & (df[flag_col] > 0)]
-    # Index properties
-    props = index.Property()
-    props.dimension = 3
-    ix3d = index.Index(properties=props)
-    # Add coordinates
-    box_size = np.array(box_size)
-    for ii in df.index:
-        coords = df.ix[ii, list(cols)].values
-        box = np.ravel([coords - box_size, coords + box_size])
-        ix3d.insert(ii, box)
-    return ix3d
 
 
 class ClusterDBSCAN(object):
